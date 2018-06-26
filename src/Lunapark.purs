e4c4cddc10ae912698971a1be1097046fc874c47
Gary Burgess <gary.burgess@gmail.com>
2018-06-26 12:13:36 +0100

    Fix warnings

--- src/Lunapark.purs
+++ src/Lunapark.purs
@@ -1,12 +1,12 @@
 module Lunapark
-  ( module API
-  , module Error
-  , module ActionF
-  , module LunaparkF
+  ( module Lunapark.API
+  , module Lunapark.Error
+  , module Lunapark.ActionF
+  , module Lunapark.LunaparkF
   ) where
 
 
-import Lunapark.API as API
-import Lunapark.Error as Error
-import Lunapark.ActionF as ActionF
-import Lunapark.LunaparkF as LunaparkF
+import Lunapark.API (BaseRun, HandleLunaparkInput, Lunapark, handleLunapark, init, interpret, interpretW3CActions, jsonWireActions, runLunapark, runLunaparkActions, w3cActions)
+import Lunapark.Error (Error, ErrorType(..), fromJson, fromStringCode, toStringCode, unknownError)
+import Lunapark.ActionF (ActionF(..), LUNAPARK_ACTIONS, TouchF(..), WithAction, _lunaparkActions, buttonDown, buttonUp, click, doubleClick, doubleTap, flick, liftAction, longTap, moveTo, pause, scroll, sendKeys, tap, touchDown, touchUp)
+import Lunapark.LunaparkF (ElementF(..), LUNAPARK, LunaparkF(..), WithLunapark, _lunapark, acceptAlert, addCookie, back, childElement, childElements, clearElement, clickElement, closeWindow, deleteAllCookies, deleteCookie, dismissAlert, elementScreenshot, executeScript, executeScriptAsync, findElement, findElements, forward, fullscreenWindow, getAlertText, getAllCookies, getAttribute, getCookie, getCss, getProperty, getRectangle, getTagName, getText, getTimeouts, getTitle, getUrl, getWindowHandle, getWindowHandles, getWindowRectangle, go, isDisplayed, isEnabled, isSelected, liftLunapark, maximizeWindow, minimizeWindow, performActions, quit, refresh, releaseActions, screenshot, sendAlertText, sendKeysElement, setTimeouts, setWindowRectangle, status, submitElement, switchToFrame, switchToParentFrame, switchToWindow)
