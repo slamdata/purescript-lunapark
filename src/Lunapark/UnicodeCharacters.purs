a559b635cff516aa7a25fa9cd455102e8840dd6a
Serhii Khoma <srghma@gmail.com>
2020-11-28 13:47:59 +0200

    feat: add UnicodeCharacters module

---
+++ src/Lunapark/UnicodeCharacters.purs
@@ -0,0 +1,157 @@
+module Lunapark.UnicodeCharacters where
+
+-- copied from https://github.com/webdriverio/webdriverio/blob/13d3a3419b951803c5e7a5db53e48764ca58f371/packages/webdriverio/src/constants.js#L315
+unicodeCharacters ::
+  { "NULL"            :: Char
+  , "Unidentified"    :: Char
+  , "Cancel"          :: Char
+  , "Help"            :: Char
+  , "Back space"      :: Char
+  , "Backspace"       :: Char
+  , "Tab"             :: Char
+  , "Clear"           :: Char
+  , "Return"          :: Char
+  , "Enter"           :: Char
+  , "Shift"           :: Char
+  , "Control"         :: Char
+  , "Control Left"    :: Char
+  , "Control Right"   :: Char
+  , "Alt"             :: Char
+  , "Pause"           :: Char
+  , "Escape"          :: Char
+  , "Space"           :: Char
+  , " "               :: Char
+  , "Pageup"          :: Char
+  , "PageUp"          :: Char
+  , "Page_Up"         :: Char
+  , "Pagedown"        :: Char
+  , "PageDown"        :: Char
+  , "Page_Down"       :: Char
+  , "End"             :: Char
+  , "Home"            :: Char
+  , "Left arrow"      :: Char
+  , "Arrow_Left"      :: Char
+  , "ArrowLeft"       :: Char
+  , "Up arrow"        :: Char
+  , "Arrow_Up"        :: Char
+  , "ArrowUp"         :: Char
+  , "Right arrow"     :: Char
+  , "Arrow_Right"     :: Char
+  , "ArrowRight"      :: Char
+  , "Down arrow"      :: Char
+  , "Arrow_Down"      :: Char
+  , "ArrowDown"       :: Char
+  , "Insert"          :: Char
+  , "Delete"          :: Char
+  , "Semicolon"       :: Char
+  , "Equals"          :: Char
+  , "Numpad 0"        :: Char
+  , "Numpad 1"        :: Char
+  , "Numpad 2"        :: Char
+  , "Numpad 3"        :: Char
+  , "Numpad 4"        :: Char
+  , "Numpad 5"        :: Char
+  , "Numpad 6"        :: Char
+  , "Numpad 7"        :: Char
+  , "Numpad 8"        :: Char
+  , "Numpad 9"        :: Char
+  , "Multiply"        :: Char
+  , "Add"             :: Char
+  , "Separator"       :: Char
+  , "Subtract"        :: Char
+  , "Decimal"         :: Char
+  , "Divide"          :: Char
+  , "F1"              :: Char
+  , "F2"              :: Char
+  , "F3"              :: Char
+  , "F4"              :: Char
+  , "F5"              :: Char
+  , "F6"              :: Char
+  , "F7"              :: Char
+  , "F8"              :: Char
+  , "F9"              :: Char
+  , "F10"             :: Char
+  , "F11"             :: Char
+  , "F12"             :: Char
+  , "Command"         :: Char
+  , "Meta"            :: Char
+  , "Zenkaku_Hankaku" :: Char
+  , "ZenkakuHankaku"  :: Char
+  }
+unicodeCharacters =
+  { "NULL":            '\xE000'
+  , "Unidentified":    '\xE000'
+  , "Cancel":          '\xE001'
+  , "Help":            '\xE002'
+  , "Back space":      '\xE003'
+  , "Backspace":       '\xE003'
+  , "Tab":             '\xE004'
+  , "Clear":           '\xE005'
+  , "Return":          '\xE006'
+  , "Enter":           '\xE007'
+  , "Shift":           '\xE008'
+  , "Control":         '\xE009'
+  , "Control Left":    '\xE009'
+  , "Control Right":   '\xE051'
+  , "Alt":             '\xE00A'
+  , "Pause":           '\xE00B'
+  , "Escape":          '\xE00C'
+  , "Space":           '\xE00D'
+  , " ":               '\xE00D'
+  , "Pageup":          '\xE00E'
+  , "PageUp":          '\xE00E'
+  , "Page_Up":         '\xE00E'
+  , "Pagedown":        '\xE00F'
+  , "PageDown":        '\xE00F'
+  , "Page_Down":       '\xE00F'
+  , "End":             '\xE010'
+  , "Home":            '\xE011'
+  , "Left arrow":      '\xE012'
+  , "Arrow_Left":      '\xE012'
+  , "ArrowLeft":       '\xE012'
+  , "Up arrow":        '\xE013'
+  , "Arrow_Up":        '\xE013'
+  , "ArrowUp":         '\xE013'
+  , "Right arrow":     '\xE014'
+  , "Arrow_Right":     '\xE014'
+  , "ArrowRight":      '\xE014'
+  , "Down arrow":      '\xE015'
+  , "Arrow_Down":      '\xE015'
+  , "ArrowDown":       '\xE015'
+  , "Insert":          '\xE016'
+  , "Delete":          '\xE017'
+  , "Semicolon":       '\xE018'
+  , "Equals":          '\xE019'
+  , "Numpad 0":        '\xE01A'
+  , "Numpad 1":        '\xE01B'
+  , "Numpad 2":        '\xE01C'
+  , "Numpad 3":        '\xE01D'
+  , "Numpad 4":        '\xE01E'
+  , "Numpad 5":        '\xE01F'
+  , "Numpad 6":        '\xE020'
+  , "Numpad 7":        '\xE021'
+  , "Numpad 8":        '\xE022'
+  , "Numpad 9":        '\xE023'
+  , "Multiply":        '\xE024'
+  , "Add":             '\xE025'
+  , "Separator":       '\xE026'
+  , "Subtract":        '\xE027'
+  , "Decimal":         '\xE028'
+  , "Divide":          '\xE029'
+  , "F1":              '\xE031'
+  , "F2":              '\xE032'
+  , "F3":              '\xE033'
+  , "F4":              '\xE034'
+  , "F5":              '\xE035'
+  , "F6":              '\xE036'
+  , "F7":              '\xE037'
+  , "F8":              '\xE038'
+  , "F9":              '\xE039'
+  , "F10":             '\xE03A'
+  , "F11":             '\xE03B'
+  , "F12":             '\xE03C'
+  , "Command":         '\xE03D'
+  , "Meta":            '\xE03D'
+  , "Zenkaku_Hankaku": '\xE040'
+  , "ZenkakuHankaku":  '\xE040'
+  }
